06cee414a3d150aa886b3c4bc7571acfd7395f22
sulta <fsultan001@citymail.cuny.edu>
2022-07-31 09:25:52 -0400

    First Commit base

---
+++ com.undecided.qa/src/main/java/com/geico/qa/base/BaseClass.java
@@ -0,0 +1,12 @@
+package com.geico.qa.base;
+
+import org.testng.annotations.BeforeMethod;
+
+public class BaseClass {
+
+	@BeforeMethod
+	public void setUp() {
+		
+	}
+
+}
---
+++ com.undecided.qa/src/main/java/com/geico/qa/utils/Configuration.java
@@ -0,0 +1,50 @@
+package com.geico.qa.utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class Configuration {
+	
+	// we will create a structure to read file
+	
+	private static Configuration configuration;
+	private static Properties properties;
+	private static String pathString = "configuration/config.properties";
+	
+	public Configuration(String path) {
+		if(path ==null) {
+			loadProperty(pathString);
+			
+		}else {
+			loadProperty(path);
+		}
+		
+		
+		
+	}
+	
+	private void loadProperty(String path) {
+		 properties = new Properties();
+		try {
+			InputStream iStream = new FileInputStream(path);
+			properties.load(iStream);
+		}catch(IOException e) {
+			
+			e.printStackTrace();
+			
+		}
+
+	}
+	
+	
+	public String getConfiguration(String key) {
+		return properties.getProperty(key);
+		
+	}
+	
+
+
+
+}
